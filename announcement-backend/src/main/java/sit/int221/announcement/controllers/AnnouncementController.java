1e3149d974de0a38578ae3a873b45aec485b81dc
RewLegendary <hongtae1011@gmail.com>
2023-08-28 14:48:57 +0700

    Update default role for backend

--- announcement-backend/src/main/java/sit/int221/announcement/controllers/AnnouncementController.java
+++ announcement-backend/src/main/java/sit/int221/announcement/controllers/AnnouncementController.java
@@ -13,7 +13,7 @@ import sit.int221.announcement.utils.Modes;
 import java.util.List;
 
 
-@CrossOrigin(origins={"http://localhost:5173","http://intproj22.sit.kmutt.ac.th/kp1","http://25.38.200.142:5173"})
+@CrossOrigin(origins={"http://localhost:5173","http://intproj22.sit.kmutt.ac.th/kp1","http://25.38.200.142:5173","http://25.37.174.170:5173"})
 @RestController
 @RequestMapping("/api/announcements")
 public class AnnouncementController {
--- announcement-backend/src/main/java/sit/int221/announcement/dtos/request/UserRequestDTO.java
+++ announcement-backend/src/main/java/sit/int221/announcement/dtos/request/UserRequestDTO.java
@@ -1,6 +1,7 @@
 package sit.int221.announcement.dtos.request;
 
 
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,10 +14,13 @@ import sit.int221.announcement.utils.enums.Role;
 public class UserRequestDTO {
 
     @NotBlank
+    @Column(unique = true)
     private String username;
     @NotBlank
+    @Column(unique = true)
     private String name;
     @NotBlank
+    @Column(unique = true)
     private String email;
 
     @EnumValidator(enumClass = Role.class,message = "must be either Role enum")
@@ -29,4 +33,8 @@ public class UserRequestDTO {
     public void setEmail(String email) { this.email = email.trim(); }
 
     public void setUsername(String username) { this.username = username.trim(); }
+
+    public Role getRole() {
+        return role == null ? Role.announcer : Role.valueOf(role);
+    }
 }
