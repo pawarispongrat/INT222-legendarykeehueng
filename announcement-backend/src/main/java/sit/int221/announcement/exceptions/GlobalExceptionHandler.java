b9822b4a53d4556d644ffc1357eeb737e1cce5e6
RewEPIC <duangcharoen.siwa@kmutt.ac.th>
2023-08-30 00:21:19 +0700

    Fix status not found from 400 to 404

--- announcement-backend/src/main/java/sit/int221/announcement/exceptions/GlobalExceptionHandler.java
+++ announcement-backend/src/main/java/sit/int221/announcement/exceptions/GlobalExceptionHandler.java
@@ -16,6 +16,7 @@ import sit.int221.announcement.exceptions.list.ItemNotFoundException;
 public class GlobalExceptionHandler {
 
     private final HttpStatus BAD_REQUEST = HttpStatus.BAD_REQUEST;
+    private final HttpStatus NOT_FOUND = HttpStatus.NOT_FOUND;
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e,WebRequest request) {
@@ -24,6 +25,14 @@ public class GlobalExceptionHandler {
             response.addValidationError(field.getField(),field.getDefaultMessage());
         });
         return ResponseEntity.status(BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(ItemNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public  ResponseEntity<ErrorResponse> handleNotFound(ItemNotFoundException e , WebRequest request){
+        ErrorResponse response = new ErrorResponse(NOT_FOUND.value(),e.getField(),getUri(request)) ;
+        response.addValidationError(e.getField(),e.getMessage());
+        return ResponseEntity.status(NOT_FOUND).body(response) ;
     }
 
     private String getUri(WebRequest request) {
--- announcement-backend/src/main/java/sit/int221/announcement/exceptions/list/ItemNotFoundException.java
+++ announcement-backend/src/main/java/sit/int221/announcement/exceptions/list/ItemNotFoundException.java
@@ -1,10 +1,16 @@
 package sit.int221.announcement.exceptions.list;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@Getter
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ItemNotFoundException extends RuntimeException {
+
+    private String field;
+
     public ItemNotFoundException(String field) {
         super(field,new Throwable("does not exists"));
+        this.field = field;
     }
 }
