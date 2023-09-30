f453b0c7f78e22a49ed5ba2b824ff82e05ef9429
RewLegendary <hongtae1011@gmail.com>
2023-09-30 18:13:35 +0700

    Remove deprecated RefreshTokenRequest

--- announcement-backend/src/main/java/sit/int221/announcement/controllers/AuthenticationController.java
+++ announcement-backend/src/main/java/sit/int221/announcement/controllers/AuthenticationController.java
@@ -2,7 +2,6 @@ package sit.int221.announcement.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import sit.int221.announcement.dtos.request.RefreshTokenRequest;
 import sit.int221.announcement.dtos.request.UserLoginDTO;
 import sit.int221.announcement.dtos.response.RefreshTokenResponse;
 import sit.int221.announcement.dtos.response.TokenResponse;
